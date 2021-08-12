34c1b97faa2e257f1951f154dd237881a89ab0f9
Etienne Eid <etienne.eid@washmen.com>
2021-08-13 00:09:50 +0300

    ⚰️ removed ConfigModule.forRoot()

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getConnectionOptions } from 'typeorm';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
       useFactory: async () =>
         Object.assign(await getConnectionOptions(), {
