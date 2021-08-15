647a466680bb10eb0ec5f94b31863dc073a30df9
Etienne Eid <etienne.eid@washmen.com>
2021-08-16 00:42:43 +0300

    ⚰️ remove controller and service

--- src/modules/availability.module.ts
+++ src/modules/availability.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AvailabilityController } from '../controllers/availability/availability.controller';
 import { AvailabilityRepository } from '../repositories/availability.repository';
-import { AvailabilityService } from '../services/availability.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([AvailabilityRepository])],
-  controllers: [AvailabilityController],
-  providers: [AvailabilityService],
 })
 export class AvailabilityModule {}
