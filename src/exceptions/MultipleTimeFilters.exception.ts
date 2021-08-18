a300c06e79272b0c5b48d656b25d65ca54b38ab7
Etienne Eid <etienne.eid@washmen.com>
2021-08-18 08:37:13 +0300

    ✏️ replace flexibility by flexible

--- src/exceptions/MultipleTimeFilters.exception.ts
+++ src/exceptions/MultipleTimeFilters.exception.ts
@@ -2,9 +2,6 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class MultipleTimeFiltersException extends HttpException {
   constructor() {
-    super(
-      'Please provide either date or flexibility',
-      HttpStatus.NOT_ACCEPTABLE,
-    );
+    super('Please provide either date or flexible', HttpStatus.NOT_ACCEPTABLE);
   }
 }
--- src/exceptions/NoTimeFilters.exception.ts
+++ src/exceptions/NoTimeFilters.exception.ts
@@ -3,7 +3,7 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 export class NoTimeFiltersException extends HttpException {
   constructor() {
     super(
-      'Please provide at least date or flexibility',
+      'Please provide at least date or flexible',
       HttpStatus.NOT_ACCEPTABLE,
     );
   }
