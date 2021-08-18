7bad35a5e1e580313cfe981667a9249616c5bd97
Etienne Eid <etienne.eid@washmen.com>
2021-08-18 15:36:52 +0300

    🚑 fix alternatives query

--- src/helpers/property/getAlternativeProperties.ts
+++ src/helpers/property/getAlternativeProperties.ts
@@ -10,6 +10,10 @@ import { FlexibilityType } from 'src/enums/flexibility-type.enum';
 import { getIntervalEndDate } from 'src/utils/getFlexibilityMonthsDates';
 import { getSortedDistinctMonths } from 'src/utils/getSortedDistinctMonths';
 import { getDiffDaysBetweenDates } from 'src/utils/getDiffDaysBetweenDates';
+import { Reservation } from 'src/entities/reservation.entity';
+import { Availability } from 'src/entities/availability.entity';
+import { addDays } from 'src/utils/addDays';
+import { formatDbDate } from 'src/utils/formatDbDate';
 
 /**
  * Returns alternative properties according to the provided filters
@@ -20,33 +24,17 @@ import { getDiffDaysBetweenDates } from 'src/utils/getDiffDaysBetweenDates';
 export const getAlternativeProperties = async (
   checkAvailabilityDto: CheckAvailabilityDto,
 ): Promise<IAlternative[] | []> => {
-  let alternativeQuery = await getManager()
-    .createQueryBuilder(Property, 'property')
-    .select(['property.id AS id', 'no.idea.what AS availableStarting'])
-    .distinct(true)
-    .innerJoin('property.building', 'building')
-    .where('building.city = :city', { city: checkAvailabilityDto.city })
-    .andWhere('property.amenities <@ :...amenities', {
-      amenities: checkAvailabilityDto.amenities,
-    });
-
-  if (checkAvailabilityDto.apartmentType) {
-    alternativeQuery = alternativeQuery.andWhere(
-      'property.propertyType = :propertyType',
-      {
-        propertyType: checkAvailabilityDto.apartmentType,
-      },
-    );
-  }
-
   // the date to start searching from
   let startDate: string;
+
   // the number of available days to search for
   let nbOfDays = 0;
+
   // the day index of the start of the weekend
   let weekendStartDayIndex = 0;
 
   if (checkAvailabilityDto.date) {
+    // the start date is the next day of the provided start date
     startDate = checkAvailabilityDto.date.start;
 
     // get the number of days the client was searching for
@@ -54,8 +42,6 @@ export const getAlternativeProperties = async (
       checkAvailabilityDto.date.start,
       checkAvailabilityDto.date.end,
     );
-    // and fetch the closest alternative
-    // starting from the start date
   } else {
     const distinctSortedMonths = getSortedDistinctMonths(
       checkAvailabilityDto.flexible.months,
@@ -77,202 +63,97 @@ export const getAlternativeProperties = async (
     }
   }
 
-  alternativeQuery = alternativeQuery
-    .leftJoin(
-      'property.reservations',
-      'reservation',
-      "reservation.checkIn >= DATE :start + interval '1 day'",
-      { start: startDate },
+  startDate = addDays(startDate, 1);
+
+  // sub-query to replace reservation3.checkout by startDate incase it's null
+  const coalesceStartDate = await getManager()
+    .createQueryBuilder(Property, 'prop2')
+    .select(`COALESCE(reservation3.checkOut + interval '1 day', :startDate)`)
+    .limit(1);
+
+  // sub-query to jump to next weekend
+  const flexibilityReservationStartDate = await getManager()
+    .createQueryBuilder(Property, 'prop')
+    .select(
+      `
+        CASE
+          WHEN NOT :weekendStartDayIndex > 0
+            THEN (${coalesceStartDate.getQuery()})
+            ELSE CASE 
+              WHEN :weekendStartDayIndex - EXTRACT(ISODOW FROM (${coalesceStartDate.getQuery()})) > 0
+              THEN (${coalesceStartDate.getQuery()}) + (:weekendStartDayIndex - EXTRACT(ISODOW FROM (${coalesceStartDate.getQuery()}))) * interval '1 day'
+              ELSE (${coalesceStartDate.getQuery()}) + (7 + :weekendStartDayIndex - EXTRACT(ISODOW FROM (${coalesceStartDate.getQuery()}))) * interval '1 day'
+            END
+        END`,
     )
-    .leftJoin(
-      'property.availabilities',
-      'availability',
-      "availability.endDate >= DATE :start + interval '1 day'",
-      { start: startDate },
-    )
-    .leftJoin(
-      'property.reservations',
-      'reservation2',
-      "reservation2.checkOut >= DATE :startDate + interval '1 day'",
-      { startDate },
-    )
+    .limit(1);
+
+  // add 1 day to the end date for the overlaps to work properly
+  const flexibilityOverlappingReservationsCount = await getManager()
+    .createQueryBuilder(Reservation, 'reservation2')
+    .select('COUNT(id)')
+    .where('reservation2.propertyId = property.id')
     .andWhere(
-      new Brackets((q1) => {
-        q1.where(
-          new Brackets((q2) => {
-            q2.where(
-              `NOT
-                  (DATE :startDate + (
-                    CASE 
-                      WHEN NOT :weekendStartDayIndex > 0 
-                        THEN 0
-                        ELSE CASE WHEN :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate) > 0 
-                          THEN :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate)
-                          ELSE 7 + :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate)
-                        END
-                    END
-                  ) * interval '1 day', :nbOfDays * interval '1 day')
-                    OVERLAPS
-                  (
-                    COALESCE(reservation.checkIn, DATE :startDate + (
-                    CASE 
-                      WHEN NOT :weekendStartDayIndex > 0 
-                        THEN 0
-                        ELSE CASE WHEN :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate) > 0 
-                          THEN :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate)
-                          ELSE 7 + :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate)
-                        END
-                    END
-                  ) * interval '1 day' + :nbOfDays * interval '1 day'),
-                    COALESCE(reservation.checkOut, DATE :startDate + (
-                      CASE 
-                        WHEN NOT :weekendStartDayIndex > 0 
-                          THEN 0
-                          ELSE CASE WHEN :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate) > 0 
-                            THEN :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate)
-                            ELSE 7 + :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate)
-                          END
-                      END
-                    ) * interval '1 day' + 2 * :nbOfDays * interval '1 day')
-                  )
-                `,
-              { startDate, nbOfDays, weekendStartDayIndex },
-            )
-              .andWhere(
-                `NOT
-                  (DATE :startDate + (
-                    CASE 
-                      WHEN NOT :weekendStartDayIndex > 0 
-                        THEN 0
-                        ELSE CASE WHEN :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate) > 0 
-                          THEN :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate)
-                          ELSE 7 + :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate)
-                        END
-                    END
-                  ) * interval '1 day', :nbOfDays * interval '1 day')
-                    OVERLAPS
-                  (
-                    COALESCE(availability.startDate, DATE :startDate + (
-                      CASE 
-                        WHEN NOT :weekendStartDayIndex > 0 
-                          THEN 0
-                          ELSE CASE WHEN :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate) > 0 
-                            THEN :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate)
-                            ELSE 7 + :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate)
-                          END
-                      END
-                    ) * interval '1 day' + :nbOfDays * interval '1 day'),
-                    COALESCE(availability.endDate, DATE :startDate + (
-                      CASE 
-                        WHEN NOT :weekendStartDayIndex > 0 
-                          THEN 0
-                          ELSE CASE WHEN :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate) > 0 
-                            THEN :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate)
-                            ELSE 7 + :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate)
-                          END
-                      END
-                    ) * interval '1 day' + 2 * :nbOfDays * interval '1 day')
-                  )
-                `,
-                { startDate, nbOfDays, weekendStartDayIndex },
-              )
-              .andWhere('availability.isBlocked = true');
-          }),
-        ).orWhere(
-          new Brackets((q3) => {
-            q3.where(
-              `NOT
-                  (
-                    COALESCE (reservation2.checkOut + interval '1 day', DATE :startDate) + (
-                      CASE 
-                        WHEN NOT :weekendStartDayIndex > 0 
-                          THEN 0
-                          ELSE CASE WHEN :weekendStartDayIndex - EXTRACT(ISODOW FROM COALESCE (reservation2.checkOut + interval '1 day', DATE :startDate)) > 0 
-                            THEN :weekendStartDayIndex - EXTRACT(ISODOW FROM COALESCE (reservation2.checkOut + interval '1 day', DATE :startDate))
-                            ELSE 7 + :weekendStartDayIndex - EXTRACT(ISODOW FROM COALESCE (reservation2.checkOut + interval '1 day', DATE :startDate))
-                          END
-                      END
-                    ) * interval '1 day',
-                    :nbOfDays * interval '1 day'
-                  ) 
-                    OVERLAPS
-                  (
-                    COALESCE(reservation.checkIn, DATE :startDate + (
-                      CASE 
-                        WHEN NOT :weekendStartDayIndex > 0 
-                          THEN 0
-                          ELSE CASE WHEN :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate) > 0 
-                            THEN :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate)
-                            ELSE 7 + :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate)
-                          END
-                      END
-                    ) * interval '1 day' + :nbOfDays * interval '1 day'),
-                    COALESCE(reservation.checkOut, DATE :startDate + (
-                      CASE 
-                        WHEN NOT :weekendStartDayIndex > 0 
-                          THEN 0
-                          ELSE CASE WHEN :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate) > 0 
-                            THEN :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate)
-                            ELSE 7 + :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate)
-                          END
-                      END
-                    ) * interval '1 day' + 2 * :nbOfDays * interval '1 day')
-                  )
-                `,
-              { startDate, nbOfDays, weekendStartDayIndex },
-            )
-              .andWhere(
-                `NOT
-                  (
-                    COALESCE (reservation2.checkOut + interval '1 day', DATE :startDate) + (
-                      CASE 
-                        WHEN NOT :weekendStartDayIndex > 0 
-                          THEN 0
-                          ELSE CASE WHEN :weekendStartDayIndex - EXTRACT(ISODOW FROM COALESCE (reservation2.checkOut + interval '1 day', DATE :startDate)) > 0 
-                            THEN :weekendStartDayIndex - EXTRACT(ISODOW FROM COALESCE (reservation2.checkOut + interval '1 day', DATE :startDate))
-                            ELSE 7 + :weekendStartDayIndex - EXTRACT(ISODOW FROM COALESCE (reservation2.checkOut + interval '1 day', DATE :startDate))
-                          END
-                      END
-                    ) * interval '1 day', 
-                    :nbOfDays * interval '1 day'
-                  )
-                    OVERLAPS
-                  (
-                    COALESCE(availability.startDate, DATE :startDate + (
-                      CASE 
-                        WHEN NOT :weekendStartDayIndex > 0 
-                          THEN 0
-                          ELSE CASE WHEN :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate) > 0 
-                            THEN :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate)
-                            ELSE 7 + :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate)
-                          END
-                      END
-                    ) * interval '1 day' + :nbOfDays * interval '1 day'),
-                    COALESCE(availability.endDate, DATE :startDate + (
-                      CASE 
-                        WHEN NOT :weekendStartDayIndex > 0 
-                          THEN 0
-                          ELSE CASE WHEN :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate) > 0 
-                            THEN :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate)
-                            ELSE 7 + :weekendStartDayIndex - EXTRACT(ISODOW FROM DATE :startDate)
-                          END
-                      END
-                    ) * interval '1 day' + 2 * :nbOfDays * interval '1 day')
-                  )
-                `,
-                { startDate, nbOfDays, weekendStartDayIndex },
-              )
-              .andWhere('availability.isBlocked = true');
-          }),
-        );
-      }),
+      `((${flexibilityReservationStartDate.getQuery()}), (:nbOfDays + 1) * interval '1 day') OVERLAPS (reservation2.checkIn, reservation2.checkOut)`,
     );
 
-  alternativeQuery = alternativeQuery.andWhere(
-    'availability.isBlocked = false',
-  );
+  const flexibilityOverlappingAvailabilityBlockedCount = await getManager()
+    .createQueryBuilder(Availability, 'availability')
+    .select('COUNT(id)')
+    .where('availability.propertyId = property.id')
+    .andWhere('availability.isBlocked = true')
+    .andWhere(
+      `((${flexibilityReservationStartDate.getQuery()}), (:nbOfDays + 1) * interval '1 day') OVERLAPS (availability.startDate, availability.endDate)`,
+    );
 
-  const matchResult = alternativeQuery.printSql().getRawMany();
+  const alternativeQuery = await getManager()
+    .createQueryBuilder(Property, 'property')
+    .select('property.id', 'id')
+    .addSelect(`(${coalesceStartDate.getQuery()})`, 'availableStarting')
+    .innerJoin('property.building', 'building');
 
-  return matchResult;
+  alternativeQuery
+    .leftJoin(
+      'property.reservations',
+      'reservation3',
+      `reservation3.checkOut >= :startDate::date`,
+      {
+        startDate,
+        nbOfDays,
+      },
+    )
+    .where(
+      `(
+      (${flexibilityOverlappingReservationsCount.getQuery()}) + 
+      (${flexibilityOverlappingAvailabilityBlockedCount.getQuery()})) = 0`,
+    )
+    .setParameters({ startDate, weekendStartDayIndex, nbOfDays })
+    .andWhere('building.city = :city', {
+      city: checkAvailabilityDto.city,
+    })
+    .andWhere(
+      'property.amenities::varchar[] @> ARRAY [:...amenities]::varchar[]',
+      {
+        amenities: checkAvailabilityDto.amenities,
+      },
+    );
+
+  if (checkAvailabilityDto.apartmentType) {
+    alternativeQuery.andWhere('property.propertyType = :propertyType', {
+      propertyType: checkAvailabilityDto.apartmentType,
+    });
+  }
+
+  const alternativeResult = await alternativeQuery
+    .orderBy(`(${coalesceStartDate.getQuery()})`, 'ASC')
+    // .groupBy('property.id')
+    .getRawMany();
+
+  // return alternativeResult;
+  return alternativeResult.map((element) => {
+    return {
+      ...element,
+      availableStarting: formatDbDate(element.availableStarting),
+    };
+  });
 };
