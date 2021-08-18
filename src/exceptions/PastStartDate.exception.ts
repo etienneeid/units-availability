262c8f930a33c35f36d33469b046cc88fd03a959
Etienne Eid <etienne.eid@washmen.com>
2021-08-18 08:37:58 +0300

    ✏️ replaced "or equal to" by "than"

--- src/exceptions/PastStartDate.exception.ts
+++ src/exceptions/PastStartDate.exception.ts
@@ -3,7 +3,7 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 export class PastStartDateException extends HttpException {
   constructor() {
     super(
-      'The start date must be greater or equal to today',
+      'The start date must be greater than today',
       HttpStatus.NOT_ACCEPTABLE,
     );
   }
